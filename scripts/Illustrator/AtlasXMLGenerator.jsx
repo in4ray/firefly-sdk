ccf5bfe8bd964a14255f5f6bc493f0c4f668e6fe
Roman Zarichnyi <roman.zarichnyi@gmail.com>
2016-03-15 22:58:30 +0200

    Updated script for Illustrator for generating FXG atlases

--- scripts/Illustrator/AtlasXMLGenerator.jsx
+++ scripts/Illustrator/AtlasXMLGenerator.jsx
@@ -1,57 +1,149 @@
 ﻿doc = app.activeDocument;
-layers = doc.layers;
-
-code = "";
-xml = new XML("<TextureAtlas/>")
-for(var i=0;i<layers.length;i++)
+var win = new Window("dialog", "Atlas Generator");
+win.browsePnl = win.add("panel", [10, 10, 440, 100]);
+win.browsePnl.add("statictext", [30, 10, 250, 30], "Add FXG files to generate atlas.");
+win.browsePnl.filesLabel = win.browsePnl.add("statictext", [30, 47, 150, 80], "Files added:");
+win.browsePnl.addButton = win.browsePnl.add("button", [300, 40 , 400, 70], "Add Files");
+win.generatePnl = win.add("panel", [10, 110, 440, 200]);
+win.generatePnl.add("statictext", [30, 10, 250, 30], "Generate FXG atlas");
+win.generatePnl.fileNameLabel = win.generatePnl.add("statictext", [30, 47, 150, 80], "File name:" + doc.fullName.name.split(".")[0] + ".xml");
+win.generatePnl.generateButton = win.generatePnl.add("button", [300, 40 , 400, 70], "Generate");
+       
+win.browsePnl.addButton.onClick = function ()
 {
-    groupItems = layers[i].groupItems;
-    for(var j=0;j<groupItems.length;j++)
+    importFiles(getFiles());
+};     
+win.generatePnl.generateButton .onClick = generateAtlas;
+
+updateNumberOfFiles();
+win.show();
+ 
+  
+function updateNumberOfFiles() 
+{
+    win.browsePnl.filesLabel.text = "Files added: "  + doc.layers[0].placedItems.length;
+}
+ 
+function getFiles() 
+{
+	return File.openDialog('Please select the files to be imported:', "*.fxg", true);
+}
+
+function importFiles(selectedFiles) 
+{	
+    selectedFiles = [].concat( selectedFiles );
+    for(var i=0;i<selectedFiles.length;i++)    
     {
-        grp = groupItems[j];
-        bounds = grp.visibleBounds;
-        for(k = grp.pathItems.length - 1; k >=0; k--){
-            if(grp.pathItems[k].typename == "PathItem" && grp.pathItems[k].clipping) 
-                  bounds = grp.pathItems[k].visibleBounds;       
-         }            
-         
-         x = bounds[0];
-         y = -bounds[1];
-         w = bounds[2] - x;
-         h = -bounds[3] - y;
-         grpName = (grp.name != 0)  ?  grp.name : "Group" + j;
-         
-         subXML = new XML("<SubTexture name='" +grpName +
-                                                          "' x='" + x  +  
-                                                          "' y='" + y  +
-                                                          "' width='" + w  +
-                                                          "' height='" + h  +
-                                                          "' frameX='0' frameY='0" +
-                                                          "' frameWidth='" + w  +
-                                                          "' frameHeight='" + h +
-                                                            "'/>");
-         
-         code += "public function get " +  grpName.substr(0, 1).toLowerCase() + grpName.substr(1, grpName.length-1) + "():Texture{ return getTextureAtlas(" + doc.name.split(".")[0] + ").getTexture(\"" + grpName + "\"); } \n";
-         
-         xml.appendChild(subXML);
+        processFile(selectedFiles[i]);
     }
+
+    updateNumberOfFiles();
 }
 
-xmlPath = doc.fullName.absoluteURI;
-parts = xmlPath.split(".");
-xmlPath= "";
-for(var i=0;i<parts.length-1;i++)
+function processFile(selectedFile) 
+{	
+   fileDocument= app.open(selectedFile);
+   assetFolder = getExtFolder();
+    if(!assetFolder.exist)
+        assetFolder.create();
+   
+   pngFilePath = assetFolder.absoluteURI + "/" + selectedFile.name + ".png";
+   opt = new ExportOptionsPNG24();
+   opt.artBoardClipping = true;
+   fileDocument.exportFile (new File(pngFilePath), ExportType.PNG24, opt);
+   fileDocument.close(SaveOptions.DONOTSAVECHANGES);
+   
+   importFileIntoLayer(new File(pngFilePath));
+}
+
+function getExtFolder() 
+{  
+    docUrl = doc.fullName.absoluteURI;
+    parts = docUrl.split("/");
+    docUrl = docUrl.substr (0, docUrl.length-parts[parts.length-1].length) + doc.name.split(".")[0] + ".assets";
+    return new Folder (docUrl);
+}
+
+function importFileIntoLayer(pngFile) 
+{	
+    firstLayer = app.activeDocument.layers[0]; 
+    itemName = pngFile.name.substring(0, pngFile.name.indexOf(".") );    
+
+    var thisPlacedItem;
+    for(var i=0;i<firstLayer.placedItems.length;i++)    
+    {
+        if(firstLayer.placedItems[i].name == itemName)
+        {
+            thisPlacedItem = firstLayer.placedItems[i];
+            break;
+        }
+    }
+    
+    if(thisPlacedItem  == null)
+        thisPlacedItem = firstLayer.placedItems.add();
+        
+    thisPlacedItem.file = pngFile;
+    thisPlacedItem.name = itemName;
+}
+
+function generateAtlas() 
 {
-    xmlPath += parts[i];
+    layers = doc.layers;
+    artboardRect = doc.artboards[0].artboardRect;
+
+    atlasName = doc.name.split(".")[0];
+    codeReg = "regFXGTextureAtlas(\"" + atlasName + "\", [";
+    code = "";
+    xml = new XML("<TextureAtlas/>");
+    //atlasName =atlasName.substr(0, 1).toLowerCase() + atlasName.substr(1, atlasName.length-1);
+    for(var i=0;i<layers.length;i++)
+    {
+        placedItems = layers[i].placedItems;
+        for(var j=0;j<placedItems.length;j++)
+        {  
+            bounds = placedItems[j].geometricBounds;
+          
+             x = bounds[0]-artboardRect[0];
+             y = -bounds[1]+artboardRect[1];
+             w = bounds[2] - x - artboardRect[0];
+             h = -bounds[3] - y + artboardRect[1];
+             itemName = (placedItems[j].name != 0)  ?  placedItems[j].name : "Group" + j;
+             
+             subXML = new XML("<SubTexture name='" +itemName +
+                                                              "' x='" + x  +  
+                                                              "' y='" + y  +
+                                                              "' width='" + w  +
+                                                              "' height='" + h  +
+                                                              "' frameX='0' frameY='0" +
+                                                              "' frameWidth='" + w  +
+                                                              "' frameHeight='" + h +
+                                                                "'/>");
+             
+             code += "public function get " +  itemName.substr(0, 1).toLowerCase() + itemName.substr(1, itemName.length-1) + "():Texture{ return getTextureAtlas(\"" + atlasName + "\").getTexture(\"" + itemName + "\"); } \n";
+             codeReg += itemName;
+             codeReg += (j <placedItems.length-1) ? ", " : "";
+             xml.appendChild(subXML);
+        }
+    }
+
+    codeReg += "], \"" + atlasName + ".xml\");";
+
+    xmlPath = doc.fullName.absoluteURI;
+    parts = xmlPath.split(".");
+    xmlPath= "";
+    for(var i=0;i<parts.length-1;i++)
+    {
+        xmlPath += parts[i];
+    }
+
+    xmlPath += ".xml";
+    file = new File (xmlPath);
+    file.open("w");
+    saved=file.writeln(xml.toXMLString() + "\n <!--\n" + codeReg + "\n\n\n" + code + "-->");
+    file.close();
+
+    if(saved)
+        Window.alert(xmlPath + " was saved.");
+    else
+        Window.alert(xmlPath + " was not saved!");
 }
-
-xmlPath += ".xml";
-file = new File (xmlPath);
-file.open("w");
-saved=file.writeln(xml.toXMLString() + "\n <!--\n" + code + "-->");
-file.close();
-
-if(saved)
-    Window.alert(xmlPath + " was saved.");
-else
-    Window.alert(xmlPath + " was not saved!");
